572a10ab8528823a07457211db377e9bb12dfc93
pedrofelixg <pedro.viniciusacm@gmail.com>
2023-02-14 19:23:17 -0300

    alteração do comportamento do botão na tag footer

---
+++ assets/newsletter.js
@@ -0,0 +1,7 @@
+const BOTAO = document.getElementById("newsletter");
+
+function assinarNewsletter() {
+    alert("Cadastro Confirmado com Sucesso");
+}
+
+BOTAO.addEventListener("click", assinarNewsletter);

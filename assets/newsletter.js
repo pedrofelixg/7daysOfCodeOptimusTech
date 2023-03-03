d8ef190e0d3ff5b5a444e37f86699fc94da8a83b
pedrofelixg <pedro.viniciusacm@gmail.com>
2023-03-03 11:03:54 -0300

    JSON stringfy add

--- assets/newsletter.js
+++ assets/newsletter.js
@@ -4,10 +4,15 @@ function assinarNewsletter() {
     const email = document.getElementById("emailsub");
     const emailValue = email.value;
 
+    const objEmail = {
+        idEmail: emailValue
+    }
+
     const regex = RegExp(/\S+@\S+\.\S+/);
 
     if(regex.test(emailValue)) {
         alert("Cadastro Confirmado");
+        JSON.stringify(objEmail, null, 4);
     } else {
         alert("Cadastro não confirmado, verifique seu e-mail");
     }
